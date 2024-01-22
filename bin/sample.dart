f29d6cc17671a19937ba4e5173c810e02d4cc15b
aryakvipin <arya28985@gmail.com>
2024-01-22 11:07:13 +0530

    Initial commit

---
+++ bin/sample.dart
@@ -0,0 +1,5 @@
+void main(){  //entry point
+  print("hallo");
+  print(25);
+  print(false);
+}
---
+++ bin/variables.dart
@@ -0,0 +1,17 @@
+void main(){
+  String name;
+   int age=25;
+   double mark=85.6;
+   name="anju";
+
+   print("my name is $name");
+   print("my age is $age");
+   print("mark = $mark");
+    name="manu";
+    age=30;
+    mark=75.3;
+    print("---------------");
+  print("my name is $name");
+  print("my age is $age");
+  print("mark = $mark");
+}
